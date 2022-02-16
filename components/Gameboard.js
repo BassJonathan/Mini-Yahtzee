af757b2d98b9ea115e3f401f73a12f2364418352
Jonathan Bass <jonathan.bass99@gmail.com>
2022-02-16 10:44:46 +0200

    Core components and style added

---
+++ components/Gameboard.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, Pressable } from 'react-native';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import styles from '../style/style';
+
+export default function Gameboard() {
+
+    return(
+        <Text>Gameboard</Text>
+    )
+}
